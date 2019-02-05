ae1777127062d7512532157a6cd02a2801cba33b
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-05 16:06:18 +0200

    Create middlewares

---
+++ middlewares/error.js
@@ -0,0 +1,29 @@
+const { HTTPError } = require('../errors')
+
+const ErrorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err)
+  }
+
+  return res.status(500).send(
+    {
+      error: { code: 500, message: err.message }
+    }
+  )
+}
+
+const HTTPErrorHandler = (err, req, res, next) => {
+  if (err instanceof HTTPError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      msg: err.message
+    })
+  }
+
+  next(err)
+}
+
+module.exports = {
+  ErrorHandler,
+  HTTPErrorHandler
+}
