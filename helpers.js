d5a9432d6501c92f05da465db72f103ce98636b1
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-06 16:20:27 +0200

    Create helpers

---
+++ helpers.js
@@ -0,0 +1,29 @@
+const uuidv4 = require('uuid/v4')
+const winston = require('winston')
+
+const { db } = require('./db')
+const { status } = require('./config/constants')
+
+const createSimpleSMPCRouter = (router, path, smpc) => {
+  router.get(path, async (req, res, next) => {
+    try {
+      const id = uuidv4()
+      const location = `/api/smpc/queue/${id}`
+
+      db.put(id, { 'status': status.PENDING })
+
+      res.set('Location', location)
+      res.status(202).json({ location })
+
+      smpc.execute()
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  return router
+}
+
+module.exports = {
+  createSimpleSMPCRouter
+}
