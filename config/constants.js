3a5027f3bda1a47cb9d998aa77e8a1d07acd495a
Christos Nasikas <xristosnasikas@gmail.com>
2019-02-06 16:19:40 +0200

    Create constants

---
+++ config/constants.js
@@ -0,0 +1,30 @@
+const status = {
+  PENDING: 0,
+  PROCESSING: 1,
+  COMPLETED: 2,
+  properties: {
+    0: { 'msg': 'pending' },
+    1: { 'msg': 'processing' },
+    2: { 'msg': 'completed' }
+  }
+}
+
+const step = {
+  COMPILING: 0,
+  RUNNING: 1,
+  OUTPUT: 2,
+  COMPLETED: 3,
+  IMPORTING: 4,
+  properties: {
+    0: { 'msg': 'Compiling mpc code' },
+    1: { 'msg': 'Running smpc computation' },
+    2: { 'msg': 'Generating output' },
+    3: { 'msg': 'Computation completed' },
+    4: { 'msg': 'Importing data' }
+  }
+}
+
+module.exports = {
+  status,
+  step
+}
