05e2201b91acb705dfceb040538b5b8c73671a84
Christos Nasikas <xristosnasikas@gmail.com>
2019-03-30 14:51:44 +0200

    Throw an error if job can not be saved

--- queue.js
+++ queue.js
@@ -1,10 +1,16 @@
 const Queue = require('bee-queue')
 const queue = new Queue('smpc')
 const { compute } = require('./smpc/smpc')
+const { HTTPError } = require('./errors')
 
 const addToQueue = ({ id }) => {
   const job = queue.createJob({ id })
   job.setId(id)
+  job.save((err, job) => {
+    if (err) {
+      throw new HTTPError(500, `Unable to save job ${job.id} on queue`)
+    }
+  })
 
   job.on('succeeded', (result) => onSucceeded(job, result))
 }
