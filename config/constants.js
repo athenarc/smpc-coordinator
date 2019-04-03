98a5acc030184eb7a81025281e7b8be0ea841eb5
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-03 15:13:18 +0300

    Refactor steps

--- config/constants.js
+++ config/constants.js
@@ -12,17 +12,23 @@ const status = {
 }
 
 const step = {
-  COMPILING: 0,
-  RUNNING: 1,
-  OUTPUT: 2,
-  COMPLETED: 3,
-  IMPORTING: 4,
+  INIT: 0,
+  COMPILE_START: 1,
+  COMPILE_END: 2,
+  IMPORT_START: 3,
+  IMPORT_END: 4,
+  COMPUTATION_START: 5,
+  COMPUTATION_END: 6,
+  COMPLETED: 6,
   properties: {
-    0: { 'msg': 'Compiling mpc code' },
-    1: { 'msg': 'Running smpc computation' },
-    2: { 'msg': 'Generating output' },
-    3: { 'msg': 'Computation completed' },
-    4: { 'msg': 'Importing data' }
+    0: { 'msg': 'Init SMPC Computation' },
+    1: { 'msg': 'Compiling MPC code' },
+    2: { 'msg': 'MPC code compilation finished' },
+    3: { 'msg': 'Importing data' },
+    4: { 'msg': 'Data Importation Finished' },
+    5: { 'msg': 'Computation started' },
+    6: { 'msg': 'Computation completed' },
+    7: { 'msg': 'Completed.' }
   }
 }
 
