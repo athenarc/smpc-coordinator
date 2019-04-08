4eabb8ca86fbc39a69634f612fece2b0de0ef9f1
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-08 15:58:08 +0300

    Pass all job details to queue

--- routes/smpc.js
+++ routes/smpc.js
@@ -1,11 +1,47 @@
 const express = require('express')
+const uuidv4 = require('uuid/v4')
 
-const { db } = require('../db')
+const { db, cachedb } = require('../db')
 const { status } = require('../config/constants')
 const { HTTPError } = require('../errors')
-const { createSimpleSMPCRouter } = require('../helpers')
+const { validateRequest, getHistogramType, sha256 } = require('../helpers')
+const { addJob } = require('../queue')
 
 let router = express.Router()
+
+const createSimpleSMPCRouter = (router, path) => {
+  router.post(path, async (req, res, next) => {
+    const results = validateRequest(req.body)
+    const algorithm = getHistogramType(req.body.attributes)
+
+    if (!results.success || algorithm === undefined) {
+      next(new HTTPError(400, results.msg))
+      return
+    }
+
+    const job = { attributes: req.body.attributes, filters: req.body.filters, algorithm }
+
+    try {
+      const id = uuidv4()
+      job.id = id
+      job.status = status.PENDING
+
+      const location = `/api/smpc/queue/${id}`
+      res.set('Location', location)
+      res.status(202).json({ location, id, status: status.properties[status.PENDING].msg })
+
+      const { status: _status, id: _id, ...rest } = job
+      await db.put(id, { ...job })
+      await cachedb.put(sha256(JSON.stringify({ ...rest })), { ...rest, id: _id, status: _status })
+
+      addJob({ ...job })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  return router
+}
 
 router.get('/queue/:id', async (req, res, next) => {
   try {
