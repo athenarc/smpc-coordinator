7c7a4e50904cf6d01d767b0b7ceba9bbe2653df0
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-08 15:56:29 +0300

    Move smpc helper

--- helpers.js
+++ helpers.js
@@ -1,44 +1,6 @@
-const uuidv4 = require('uuid/v4')
-const winston = require('winston')
 const _ = require('lodash')
 const crypto = require('crypto')
 
-const { HTTPError } = require('./errors')
-const { status } = require('./config/constants')
-const { db } = require('./db')
-
-const totalAttributes = require('./attributes.json')
-
-const createSimpleSMPCRouter = (router, path) => {
-  const { addToQueue } = require('./queue')
-
-  router.post(path, async (req, res, next) => {
-    const results = validateRequest(req.body)
-
-    if (!results.success) {
-      next(new HTTPError(400, results.msg))
-      return
-    }
-
-    const job = { attributes: req.body.attributes, filters: req.body.filters }
-
-    try {
-      const id = uuidv4()
-      job.id = id
-      job.status = status.PENDING
-      const location = `/api/smpc/queue/${id}`
-
-      res.set('Location', location)
-      res.status(202).json({ location, id, status: status.properties[status.PENDING].msg })
-      await db.put(id, { 'status': status.PENDING })
-      addToQueue({ ...job })
-    } catch (err) {
-      next(err)
-    }
-  })
-
-  return router
-}
 const totalAttributes = require('./smpc-global/attributes.json')
 const algorithms = require('./smpc-global/algorithms.json')
 
