826671a427dc1117cf790adb2dd81f289548957b
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-12 17:47:59 +0300

    Add session middleware

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 
 const LISTEN_PORT = process.env.PORT || 3000
+const ENV = process.env.NODE_ENV || 'development'
 
 const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
 const cors = require('cors')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
 const routes = require('./routes')
 const queue = require('./queue')
 const logger = require('./config/winston')
@@ -25,6 +28,16 @@ if (_.isEmpty(process.env.SMPC_ENGINE)) {
   app.use(helmet())
   app.use(cors({ methods: ['GET', 'POST'] }))
   app.use(bodyParser.json())
+
+  const sessionMiddleware = session({
+    store: new RedisStore(),
+    secret: process.env.SESSION_SECRET || 'smpc-coordinator session secret',
+    resave: false,
+    cookie: { secure: ENV !== 'development' },
+    saveUninitialized: true // investigate more the option
+  })
+
+  app.use(sessionMiddleware)
 
   for (const url in routes) {
     app.use(url, routes[url])
