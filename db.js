aed02cd4804530cfbd20a7528c577cf8662e2901
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-14 18:09:46 +0300

    Remove cache

--- db.js
+++ db.js
@@ -1,18 +1,14 @@
 const level = require('level')
-const { sha256 } = require('./helpers')
 const { appEmitter } = require('./emitters.js')
 
 const db = level('./smpc-db', { valueEncoding: 'json' })
-const cachedb = level('./smpc-cache', { valueEncoding: 'json' })
 
 appEmitter.on('update-computation', (msg) => {
   updateJob(msg)
 })
 
 const addJobToDB = async (job) => {
-  const { status: _status, id: _id, ...rest } = job
   await db.put(job.id, { ...job })
-  await cachedb.put(sha256(JSON.stringify({ ...rest })), { ...rest, id: _id, status: _status })
 }
 
 const updateJob = async (job) => {
