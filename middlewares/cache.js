0b8540dca540369c9a674f04593b45840315cf78
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-14 18:11:03 +0300

    Create cache middleware

---
+++ middlewares/cache.js
@@ -0,0 +1,33 @@
+const uuidv4 = require('uuid/v4')
+const { getFromCache } = require('../cache')
+const { sha256 } = require('../helpers')
+const { getJob, addJobToDB } = require('../db')
+
+const cache = async (req, res, next) => {
+  const { id: _id, ...rest } = req.body
+  const key = sha256(JSON.stringify(rest))
+
+  try {
+    let cacheRes = await getFromCache(key)
+
+    if (cacheRes) { // cache hit
+      cacheRes = JSON.parse(cacheRes)
+      const dbRes = await getJob(cacheRes.id)
+      const newID = uuidv4()
+      await addJobToDB({ ...dbRes, id: newID, timestamps: { accepted: Date.now(), done: Date.now() }, fromCache: true })
+
+      const location = `/api/smpc/results/${newID}`
+
+      res.set('Location', location)
+      return res.status(303).json({ location, ...cacheRes })
+    } else { // cache miss
+      next()
+    }
+  } catch (e) {
+    console.log(`Cache error: ${e}`)
+  }
+
+  next()
+}
+
+module.exports = cache
