10e474636a601a7d8f84897a37cd55c5db320eeb
Christos Nasikas <xristosnasikas@gmail.com>
2019-05-06 18:03:13 +0300

    Create users entries

---
+++ db/users.js
@@ -0,0 +1,39 @@
+module.exports = [
+  {
+    id: 0,
+    username: 'admin',
+    email: 'admin@admin.com',
+    password: '78dc2824bab0374873a8b346929f21cff23c4450ff2aa369121db84d3362d339',
+    salt: '$2b$10$VVZBpN0BEtH1dsZjBZ/xfu',
+    hash: '$2b$10$VVZBpN0BEtH1dsZjBZ/xfui0xa/kgdciQWFSKRy6EtvAglcW/l5HK',
+    role: 'admin'
+  },
+  {
+    id: 1,
+    username: 'laxmana',
+    email: 'xnasikas@di.uoa.gr',
+    password: '580ed8ebdbc2b32a1842c1cdb7baba1431d6955a4ea85bccaa950bc70d49fdde',
+    role: 'admin'
+  },
+  {
+    id: 2,
+    username: 'gpik',
+    email: 'gpik@di.uoa.gr',
+    password: 'e301957a0c252e5073688fa3e698dc20dd6a7814abda7990375d193b46f60eaa',
+    role: 'admin'
+  },
+  {
+    id: 3,
+    username: 'test_request',
+    email: 'test_request@di.uoa.gr',
+    password: 'f1afe288b334f5fa2a3716916826aec3a1db7162c2cc3090e52d28bbdfacab33',
+    role: 'requestor'
+  },
+  {
+    id: 4,
+    username: 'test_viewer',
+    email: 'test_viewer@di.uoa.gr',
+    password: 'bdcc1fc0951a775c3c2ca0b8a45b8c44a814d130b80eded9a29cab92ad1e0427',
+    role: 'viewer'
+  }
+]
