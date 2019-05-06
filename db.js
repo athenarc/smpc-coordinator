d2831b7f72848cc6beddd4f3b337c5435939361a
Christos Nasikas <xristosnasikas@gmail.com>
2019-05-06 18:03:39 +0300

    Small refactor

--- db.js
+++ db.js
@@ -5,9 +5,22 @@ const { appEmitter } = require('./emitters.js')
 const db = level(path.resolve(__dirname, './smpc-db'), { valueEncoding: 'json' })
 const userDB = level(path.resolve(__dirname, './smpc-user-db'), { valueEncoding: 'json' })
 
+const databases = {
+  'smpc': db,
+  'users': userDB
+}
+
 appEmitter.on('update-computation', (msg) => {
   updateJob(msg)
 })
+
+const add = (db, key, value) => {
+  return databases[db].put(key, { ...value })
+}
+
+const get = (db, key) => {
+  return databases[db].get(key)
+}
 
 const addJobToDB = async (job) => {
   await db.put(job.id, { ...job })
@@ -22,9 +35,21 @@ const getJob = async (id) => {
   return out
 }
 
+const getUser = async id => {
+  const out = await get('users', id)
+  return out
+}
+
+const addUser = async user => {
+  await add('user', user.username, user)
+}
+
 module.exports = {
   db,
+  userDB,
   addJobToDB,
   updateJob,
-  getJob
+  getJob,
+  getUser,
+  addUser
 }
