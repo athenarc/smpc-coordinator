eee391d5a717b2dec3a764e29c7be98044e7bfb5
Christos Nasikas <xristosnasikas@gmail.com>
2019-05-17 18:36:12 +0300

    Remove level

--- config/winston.js
+++ config/winston.js
@@ -1,7 +1,6 @@
 const { createLogger, format, transports } = require('winston')
 
 const logger = createLogger({
-  level: 'info',
   format: format.combine(
     format.colorize(),
     format.splat(),
