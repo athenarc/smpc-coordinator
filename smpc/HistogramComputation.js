a6636f5f7aeb14caf79093df4369fb45edb6b24a
Christos Nasikas <xristosnasikas@gmail.com>
2019-06-21 01:27:13 +0300

    Get attribute names from mesh

--- smpc/HistogramComputation.js
+++ smpc/HistogramComputation.js
@@ -86,19 +86,16 @@ class HistogramComputation extends Computation {
     return arr
   }
 
-  getAttributeNames (name) {
-    const terms = mapping[name]
-    const names = []
-
-    for (var term in terms) {
-      if (terms.hasOwnProperty(term)) {
-        if (meshTerms[term]) {
-          names.push(meshTerms[term].name)
-        }
-      }
-    }
-
-    return names
+  getAttributeNames (mesh) {
+    // Take the values of the childer of the attribute
+    // Sort children by mapping number
+    // Get only mesh term
+    // Get mesh's term name
+    return Object
+      .entries(mapping[mesh])
+      .sort((a, b) => a[1] - b[1])
+      .map(t => t[0])
+      .map(m => meshTerms[m].name)
   }
 
   computeAxisLabels (min, max, width, cells) {
@@ -122,7 +119,6 @@ class HistogramComputation extends Computation {
       results = data.reduce((previous, current) => {
         const xy = current.replace(/\s/g, '').split(',')
         previous.y.push(Number(xy[1]))
-
         return previous
       }, { x: this.getAttributeNames(this.job.data.attributes[0].name), y: [] })
     }
