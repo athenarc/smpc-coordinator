65cf8bec7364d0f0803dd8488f1bdc27132c3676
Christos Nasikas <xristosnasikas@gmail.com>
2019-06-29 14:29:18 +0300

    Better messages

--- middlewares/attributes.js
+++ middlewares/attributes.js
@@ -3,7 +3,7 @@ const { HTTPError } = require('../errors')
 
 const processAttributes = (req, res, next) => {
   if (!req.body.attributes || !_.isArray(req.body.attributes)) {
-    next(new HTTPError(400, 'Bad request'))
+    next(new HTTPError(400, 'attributes: Bad request'))
     return
   }
 
