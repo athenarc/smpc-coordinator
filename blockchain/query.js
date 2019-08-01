1fcb82cc550317308d40c0d608128777f55b4ee4
Christos Nasikas <xristosnasikas@gmail.com>
2019-08-01 16:13:40 +0300

    Fix linting errors

--- blockchain/query.js
+++ blockchain/query.js
@@ -1,15 +1,15 @@
 /*
-* Copyright IBM Corp All Rights Reserved
-*
-* SPDX-License-Identifier: Apache-2.0
-*/
+ * Copyright IBM Corp All Rights Reserved
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 /*
  * Chaincode query
  */
 
 const util = require('util')
 
-const isJSON = (str) => {
+const isJSON = str => {
   try {
     JSON.parse(str)
   } catch (e) {
@@ -25,42 +25,56 @@ module.exports = {
     let response = ''
     let ret = ''
 
-    return channel.queryByChaincode(request).then((queryResponses) => {
-          // console.log("Query has completed, checking results");
-          // queryResponses could have more than one  results if there multiple peers were used as targets
-          // console.log(queryResponses[0].response.status);
-      if (queryResponses && queryResponses.length === 1) {
-        if (queryResponses[0] instanceof Error) {
-          status = '500'
-          message = queryResponses[0]
-          // console.error("error from query = ", queryResponses[0]);
+    return channel
+      .queryByChaincode(request)
+      .then(queryResponses => {
+        // console.log("Query has completed, checking results");
+        // queryResponses could have more than one  results if there multiple peers were used as targets
+        // console.log(queryResponses[0].response.status);
+        if (queryResponses && queryResponses.length === 1) {
+          if (queryResponses[0] instanceof Error) {
+            status = '500'
+            message = queryResponses[0]
+            // console.error("error from query = ", queryResponses[0]);
+          } else {
+            // console.log(util.format( '{status : %s response : "%s" }', status, queryResponses[0].toString()));
+            response = queryResponses[0].toString()
+          }
         } else {
-          // console.log(util.format( '{status : %s response : "%s" }', status, queryResponses[0].toString()));
-          response = queryResponses[0].toString()
-        }
-      } else {
           // console.log("No payloads were returned from query");
 
+          status = '500'
+          message = 'no payload returned from query'
+        }
+      })
+      .catch(err => {
         status = '500'
-        message = 'no payload returned from query'
-      }
-    }).catch((err) => {
-      status = '500'
-      message = err
+        message = err
         // console.error('Failed to query successfully :: ' + err);
-    }).then(() => {
-      let msg = ''
-      if (message.message) {
-        msg = message.message
-      }
+      })
+      .then(() => {
+        let msg = ''
+        if (message.message) {
+          msg = message.message
+        }
 
-      if (isJSON(response)) {
-        ret = util.format('{"status" : "%s", "response" : %s, "message" : %s }', status, response, JSON.stringify(msg))
-      } else {
-        ret = util.format('{"status" : "%s", "response" : %s, "message" : %s }', status, JSON.stringify(response), JSON.stringify(msg))
-      }
+        if (isJSON(response)) {
+          ret = util.format(
+            '{"status" : "%s", "response" : %s, "message" : %s }',
+            status,
+            response,
+            JSON.stringify(msg)
+          )
+        } else {
+          ret = util.format(
+            '{"status" : "%s", "response" : %s, "message" : %s }',
+            status,
+            JSON.stringify(response),
+            JSON.stringify(msg)
+          )
+        }
 
-      return ret
-    })
+        return ret
+      })
   }
 }
