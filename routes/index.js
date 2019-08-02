e92735007590f25be4a6cb89c69efe412d785ee0
Christos Nasikas <xristosnasikas@gmail.com>
2019-08-02 17:08:26 +0300

    Create auth route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const smpc = require('./smpc')
 const search = require('./search')
+const auth = require('./auth')
 
 const router = express.Router()
 
@@ -10,5 +11,6 @@ router.get('/health', (_, res) => res.status(204).send())
 module.exports = {
   '/api': router,
   '/api/smpc': smpc,
-  '/api/search': search
+  '/api/search': search,
+  '/api/auth': auth
 }
